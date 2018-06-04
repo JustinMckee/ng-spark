5763ffbb5dfe2132aaea694025fbc54df5dd1fe0
Justin McKee <justinmckee@Justins-MacBook-Pro.local>
2018-06-03 17:48:30 -0700

    Adds Material design and updates angular cli, angular

---
+++ src/app/material-theme/material-theme.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  declarations: []
+})
+export class MaterialThemeModule { }
